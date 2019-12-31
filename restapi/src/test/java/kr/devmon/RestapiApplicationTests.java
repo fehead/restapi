61cca5c3a1393756988f8c9a37402f00745e6533
chaehoon <chaehoon.leem@devmon.co.kr>
2019-12-31 19:27:59 +0900

    first import project.

---
+++ restapi/src/test/java/kr/devmon/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.devmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
