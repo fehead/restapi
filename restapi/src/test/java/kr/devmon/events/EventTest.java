84f926d300120d7baf01c380957e0a29e02bf2f8
fehead <im.fehead@gmail.com>
2020-01-04 22:26:48 +0900

    Event domain.

---
+++ restapi/src/test/java/kr/devmon/events/EventTest.java
@@ -0,0 +1,31 @@
+package kr.devmon.events;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class EventTest {
+
+	@Test
+	void builder() {
+		Event event = Event.builder()
+				.name("fehead's STring REST API")
+				.description("Rest API development with String")
+				.build();
+		assertThat(event).isNotNull();
+
+	}
+
+	@Test
+	void javaBean() {
+		Event event = new Event();
+		String name = "fehead's STring REST API";
+		String description = "Rest API development with String";
+		event.setName(name);
+		event.setDescription(description);
+
+		assertThat(event.getName()).isEqualTo(name);
+		assertThat(event.getDescription()).isEqualTo(description);
+	}
+
+}
