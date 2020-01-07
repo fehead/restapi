3a86dbd85e337bbb39e16517b6d8ae0f38d5dcb5
fehead <im.fehead@gmail.com>
2020-01-07 16:22:41 +0900

    EventConrollerTest spring boot 2.1.0 에 맞게 수정.

--- restapi/src/test/java/kr/devmon/events/EventControllerTest.java
+++ restapi/src/test/java/kr/devmon/events/EventControllerTest.java
@@ -4,25 +4,27 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+// @SpringBootTest
+@RunWith(SpringRunner.class)
 @WebMvcTest
 public class EventControllerTest {
 	@Autowired
 	MockMvc	mockMvc;
-	
+
 	@Test
 	void createEvent() throws Exception {
-		mockMvc.perform(post("/api/event/")
-				.contentType(MediaType.APPLICATION_JSON)
+		mockMvc.perform(post("/api/events/")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
 				.accept(MediaTypes.HAL_JSON)
-				)			
+				)
 			.andExpect(status().isCreated());
 	}
 }
